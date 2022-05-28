2bc80e81f899f2e51f4c33725c9f92efa4d388ef
lzampieri <zampieri.leonardo98@gmail.com>
2022-05-29 00:20:22 +0200

    Version 1.0.2

--- compress.bat
+++ compress.bat
@@ -3,7 +3,7 @@ npm run build & ^
 cd .. & ^
 npm run prod & ^
 rmdir alumnisgss_theme /s /q & ^
-robocopy . alumnisgss_theme /s /xd *node_modules* /xf *.json /xf *.bat /xf safelist.txt /xf tailwind.config.js /xf theme.js /xf webpack.*.js /xd alumnisgss_theme /xd src /xf *.jsx /xf .babelrc & ^
+robocopy . alumnisgss_theme /s /xd *node_modules* /xd *.git* /xf *.json /xf *.bat /xf safelist.txt /xf tailwind.config.js /xf theme.js /xf webpack.*.js /xd alumnisgss_theme /xd src /xf *.jsx /xf .babelrc & ^
 del alumnisgss_theme.zip & ^
 tar -acf alumnisgss_theme.zip alumnisgss_theme & ^
 rmdir alumnisgss_theme /s /q
